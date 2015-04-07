a8d9bd2668d1ce4b0fe8f21eee233dbe7245fbb9
Jeremy Woertink <jeremywoertink@gmail.com>
2015-04-06 20:24:21 -0700

    added a new javascript file

---
+++ script.js
@@ -0,0 +1,11 @@
+var name = "Jeremy";
+var welcome = "Welcome, " + name;
+alert(welcome);
+
+var tacoDiv = document.getElementById("taco");
+tacoDiv.style.backgroundColor = "red";
+tacoDiv.style.color = "white";
+tacoDiv.style.textAlign = "center";
+tacoDiv.style.padding = "15px";
+tacoDiv.style.fontSize = "85px";
+console.log(tacoDiv);
